86f14ac4f34810ceb3919a40988aeebb377a0f6f
Nguyễn Quý Triều <quytrieu.nguyen@outlook.com>
2021-05-18 15:38:29 +0700

    them doanh thu danh sách user user detail

---
+++ PBL3Store.UI/Models/Dynamic/BookRevenue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PBL3Store.UI.Models.Dynamic
+{
+    public class BookRevenue
+    {
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
