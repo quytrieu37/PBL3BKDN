616f9a936d054dfb353added0f370945969f3ec5
Nguyễn Quý Triều <quytrieu1379@gmail.com>
2021-07-31 15:28:50 +0700

    them generic repository da bind

---
+++ PBL3Store.Domain/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBL3Store.Domain.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetALl();
+        void Add(T obj);
+        void Delete(T obj);
+        void DeleteById(int Id);
+        void Edit(T obj);
+        void SaveChange();
+    }
+}
